b3139341942dda0dbeed4c174d4304437e84d727
fabien-design <fabienrozier60@gmail.com>
2023-11-08 10:46:58 +0100

    base 4 couches

--- InfirmerieDAL/InfirmerieDAO.cs
+++ InfirmerieDAL/InfirmerieDAO.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
 using InfirmerieBO;
 
 namespace InfirmerieDAL
